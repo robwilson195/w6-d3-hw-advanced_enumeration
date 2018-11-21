6b6bf598d2bcd4b8799ee05d811ad5c77daf3424
Rob Wilson <rob.wilson195@gmail.com>
2018-11-21 17:32:47 +0000

    Completed pangram.

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,11 +1,11 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-  this.phrase = phrase;
+  this.phrase = phrase.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
   return this.alphabet.every((letter) => {
-    return this.phrase.split('').some((phraseLetter) => {
+    return this.phrase.some((phraseLetter) => {
       return letter.toUpperCase() === phraseLetter.toUpperCase();
     });
   });
