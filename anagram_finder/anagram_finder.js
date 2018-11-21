a845a4d2a2e34e20539ec55d215fea47b8cd30f2
Rob Wilson <rob.wilson195@gmail.com>
2018-11-21 22:15:13 +0000

    Wrote a couple of functions, but not working yet.

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,29 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.toUpperCase().split('');
 }
 
-AnagramFinder.prototype.findAnagrams = function (otherWords) {
+AnagramFinder.prototype.countLetters = function (wordToCount) {
+  const letterCount = {};
+  wordToCount.split('').forEach((letter) => {
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  });
+  return letterCount;
+};
 
+AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  return otherWords.filter((checkWord) => {
+    let result = false;
+    if (checkWord.split('') !== this.word) {
+      if (this.countLetters(checkWord) === this.countLetters(this.word.join(''))) {
+        result = true;
+      }
+    }
+    return result;
+  });
 }
 
 module.exports = AnagramFinder;
