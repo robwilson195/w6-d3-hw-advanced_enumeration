a329cd7239580cced5de4742939177a8d5979923
Rob Wilson <rob.wilson195@gmail.com>
2018-11-21 17:07:18 +0000

    Completed upper_caser.

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,11 @@
 const UpperCaser = function (words) {
+  this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+  return this.words.map((lowerCaseWord) => {
+    return lowerCaseWord.toUpperCase();
+  });
 }
 
 module.exports = UpperCaser;
