0b9b6c05b02a74f88489b5ed83a5c16077e9e3d7
Rob Wilson <rob.wilson195@gmail.com>
2018-11-21 21:12:15 +0000

    Added a function and test to count the number of a given letter in the word.

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,17 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
+
+IsogramFinder.prototype.quantityOfLetter = function (searchLetter) {
+  return this.word.reduce((accum, letter) => {
+    if (letter === searchLetter) {
+      return accum + 1;
+    } return accum;
+  }, 0);
+};
 
 IsogramFinder.prototype.isIsogram = function () {
 
-}
+};
 
 module.exports = IsogramFinder;
